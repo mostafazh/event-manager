c243d950327dcbb5ca760c68c91e1a40d4fb0f57
mostafa.zhosney@gmail.com <mostafa.zhosney@gmail.com@afc5f4bc-9dfc-ef68-4742-a1a4af2de0d5>
2010-12-05 20:25:30 +0000

    added Google Maps Support

--- trunk/android/guest/src/eg/edu/guc/hci/guest/ui/MapsActivity.java
+++ trunk/android/guest/src/eg/edu/guc/hci/guest/ui/MapsActivity.java
@@ -1,13 +1,22 @@
 package eg.edu.guc.hci.guest.ui;
 
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
 import eg.edu.guc.hci.guest.ui.R;
-import android.app.Activity;
 import android.os.Bundle;
 
-public class MapsActivity extends Activity {
+public class MapsActivity extends MapActivity {
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.maps);
+		MapView mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
 	}
 }
