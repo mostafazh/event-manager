a015902fe87671308f78d67e8002c178c2f3bb8e
mostafa.zhosney@gmail.com <mostafa.zhosney@gmail.com@afc5f4bc-9dfc-ef68-4742-a1a4af2de0d5>
2010-12-05 18:44:02 +0000

    Initial import.

---
+++ trunk/android/usher/src/eg/edu/guc/hci/ui/ReportsActivity.java
@@ -0,0 +1,16 @@
+package eg.edu.guc.hci.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ReportsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		TextView textview = new TextView(this);
+		textview.setText("This is the Reports tab");
+		setContentView(textview);
+	}
+}
