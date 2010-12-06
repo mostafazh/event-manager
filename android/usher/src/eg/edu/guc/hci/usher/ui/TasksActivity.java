a664fc440ee42a9ad9ede9f9b0841a5f142856c3
ahmed.nagah.elsawy@gmail.com <ahmed.nagah.elsawy@gmail.com@afc5f4bc-9dfc-ef68-4742-a1a4af2de0d5>
2010-12-06 20:12:05 +0000

    gomaa is merging

--- android/usher/src/eg/edu/guc/hci/usher/ui/TasksActivity.java
+++ android/usher/src/eg/edu/guc/hci/usher/ui/TasksActivity.java
@@ -4,17 +4,9 @@ package eg.edu.guc.hci.usher.ui;
 
 
 import android.app.ListActivity;
-import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,14 +15,14 @@ import android.widget.ListView;
 public class TasksActivity extends ListActivity {
     /** Called when the activity is first created. */
     
-	static String msg1 = "Task1\nTask1 description";
+	static String msg1 = "Task1\nTask1 killing guests";
 	static String msg2 = "Task2\nTask1 description";
 	static String msg3 = "Task3\nTask1 description";
 	static String msg4 = "Task4\nTask1 description";
 	
 	
 	
-    static final String[] msgs = new String[] {msg1,msg2,msg3,msg1};
+    static final String[] msgs = new String[] {msg1};//,msg2,msg3,msg1};
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	  super.onCreate(savedInstanceState);
@@ -43,7 +35,7 @@ public class TasksActivity extends ListActivity {
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) 
     {
-//    	startActivity(new Intent(this, Task1Activity.class));
+    	startActivity(new Intent(this, task1Activity.class));
     }
     
 }
