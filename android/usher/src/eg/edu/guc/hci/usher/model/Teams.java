4c9f4122e52bbd1315424485b6a2466ea28482ab
ahmed.nagah.elsawy@gmail.com <ahmed.nagah.elsawy@gmail.com@afc5f4bc-9dfc-ef68-4742-a1a4af2de0d5>
2010-12-06 20:25:15 +0000

    zaher is merging .... ostor ya rab

--- android/usher/src/eg/edu/guc/hci/usher/model/Teams.java
+++ android/usher/src/eg/edu/guc/hci/usher/model/Teams.java
@@ -1,9 +1,10 @@
 package eg.edu.guc.hci.usher.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Teams {
-	private ArrayList<Team> teams = new ArrayList<Team>();
+	private List<Team> teams = new ArrayList<Team>();
 
 	public Teams() {
 		Team team1 = new Team("Team 1");
@@ -20,7 +21,15 @@ public class Teams {
 		teams.add(team2);
 	}
 
-	public ArrayList<Team> getTeams() {
+	public List<Team> getTeams() {
 		return teams;
+	}
+	
+	public List<TeamMember> getTeamsMembers() {
+		List<TeamMember> tm = new ArrayList<TeamMember>();
+		for(int i=0;i<teams.size();i++){
+			tm.addAll(teams.get(i).getMembers());
+		}
+		return tm;
 	}
 }
